32c71f9cb068446d646c20a77a2539e2a5806763
Vadim Maslov <mvadim@me.com>
2014-10-19 16:17:48 +0300

    hide status bar, update autolayout, add clear button

--- QR Generator/AppDelegate.m
+++ QR Generator/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:NO];
+
     // Override point for customization after application launch.
     return YES;
 }
--- QR Generator/ViewController.m
+++ QR Generator/ViewController.m
@@ -18,29 +18,44 @@
 
 @implementation ViewController
 
-
 //hide keyboard by typing on screen
 - (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [self.view endEditing:YES];
 }
 
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
 - (IBAction)generate:(UIButton *)sender
 {
     if ([self.codeTextField.text length] == 8) {
-        [self.view endEditing:YES];
-
-        NSString *str = [NSString stringWithFormat:@"MAYKOR_ASSET_QR_ID_%ld_%ld",
-                         (long)[[self checkString:self.codeTextField.text] integerValue],
-                         [[self checkString:self.codeTextField.text] integerValue]*3];
-        self.codeLabel.text = str;
-        QRImage *qrimage = [[QRImage alloc] initWithString:str];
-        self.qrImageView.image = qrimage.image;
+        [self.codeTextField resignFirstResponder];
+        NSString *str           = [NSString stringWithFormat:@"MAYKOR_ASSET_QR_ID_%ld_%ld",
+                                   (long)[[self checkString:self.codeTextField.text] integerValue],
+                                   [[self checkString:self.codeTextField.text] integerValue]*3];
+        self.codeLabel.text     = str;
+        QRImage *qrimage        = [[QRImage alloc] initWithString:str];
+        self.qrImageView.image  = qrimage.image;
     } else {
-       [[ [UIAlertView alloc] initWithTitle:@"Ошибка!" message:@"Введите восемь цифр." delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil]show];
+        [[ [UIAlertView alloc] initWithTitle:@"Ошибка!"
+                                     message:@"Введите восемь цифр."
+                                    delegate:nil
+                           cancelButtonTitle:@"Ok"
+                           otherButtonTitles:nil, nil]show];
     }
 }
 
+- (IBAction)clear:(UIButton *)sender
+{
+    self.codeTextField.text = @"";
+    self.codeLabel.text     = @"";
+    self.qrImageView.image  = nil;
+}
+
+//As notice in requirements we should remove zero from beginning
 -(NSString *)checkString:(NSString *)str
 {
     while ([str characterAtIndex:0] == '0') {
